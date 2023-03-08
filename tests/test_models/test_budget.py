268b31b6e6ecd9269350ea48db10cba203491cd3
djcucumber <anytka.moxova2023@yandex.ru>
2023-03-09 01:07:05 +0300

    Начало разработки тестов для модели "бюджет"

--- tests/test_models/test_budget.py
+++ tests/test_models/test_budget.py
@@ -1,3 +1,15 @@
 """
 Тесты для бюджета
 """
+
+from datetime import datetime
+
+import pytest
+
+from bookkeeper.repository.memory_repository import MemoryRepository
+from bookkeeper.models.budget import Budget
+
+@pytest.fixture
+def repo():
+    return MemoryRepository()
+
