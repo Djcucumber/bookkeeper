b744aa8ea24d1dbca2e400a684711d131df0b096
djcucumber <anytka.moxova2023@yandex.ru>
2023-03-11 23:30:32 +0300

    Добавлены тесты для test_budget.py

--- tests/test_models/test_budget.py
+++ tests/test_models/test_budget.py
@@ -2,14 +2,30 @@
 Тесты для бюджета
 """
 
-from datetime import datetime
-
 import pytest
 
 from bookkeeper.repository.memory_repository import MemoryRepository
 from bookkeeper.models.budget import Budget
 
+
 @pytest.fixture
 def repo():
     return MemoryRepository()
 
+
+def test_create_with_full_args_list():
+    e = Budget(term="day", amount=100, category=1, pk=1)
+    assert e.term == "day"
+    assert e.amount == 100
+
+
+def test_create_brief():
+    e = Budget("day", 100, 1)
+    assert e.term == "day"
+    assert e.amount == 100
+
+
+def test_can_add_to_repo(repo):
+    e = Budget("day", 100, 1)
+    pk = repo.add(e)
+    assert e.pk == pk
