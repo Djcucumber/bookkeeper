200e8b9cd3ba78bb3002437d809fda3a1b6842b0
djcucumber <anytka.moxova2023@yandex.ru>
2023-03-11 19:01:39 +0300

    Добавлены тесты для sqlite_repository.py

--- tests/test_repository/test_sqlite_repository.py
+++ tests/test_repository/test_sqlite_repository.py
@@ -5,7 +5,7 @@ import pytest
 @pytest.fixture
 def custom_class():
     class Custom():
-        pk = 0
+        pk: int = 0
 
     return Custom
 
@@ -13,11 +13,22 @@ def custom_class():
 @pytest.fixture
 def repo():
     return SQLiteRepository()
-"""
-Тесты на проверку pk
-"""
 
-def test_cannot_add_with_pk(db_file, cls):
+
+def test_crud(repo, custom_class):
+    obj = custom_class()
+    pk = repo.add(obj)
+    assert obj.pk == pk
+    assert repo.get(pk) == obj
+    obj2 = custom_class()
+    obj2.pk = pk
+    repo.update(obj2)
+    assert repo.get(pk) == obj2
+    repo.delete(pk)
+    assert repo.get(pk) is None
+
+
+def test_cannot_add_with_pk(db_file, custom_class):
     """
     Тесты на проверку pk
     """
@@ -26,33 +37,31 @@ def test_cannot_add_with_pk(db_file, cls):
     with pytest.raises(ValueError):
         repo.add(obj)
 
-def test_cannot_add_without_pk(db_file, cls):
+
+def test_cannot_add_without_pk(db_file, custom_class):
     with pytest.raises(ValueError):
         repo.add(0)
 
-def test_cannot_delete_unexistent(db_file, cls):
+
+def test_cannot_delete_unexistent(db_file, custom_class):
     with pytest.raises(KeyError):
         repo.delete(1)
 
-def test_cannot_delete_unexistent(db_file, cls):
-    with pytest.raises(KeyError):
-        repo.delete(1)
 
-
-def test_cannot_update_without_pk(db_file, cls):
+def test_cannot_update_without_pk(db_file, custom_class):
     obj = custom_class()
     with pytest.raises(ValueError):
         repo.update(obj)
 
 
-def test_get_all(db_file, cls):
+def test_get_all(db_file, custom_class):
     objects = [custom_class() for i in range(5)]
     for o in objects:
         repo.add(o)
     assert repo.get_all() == objects
 
 
-def test_get_all_with_condition(db_file, cls):
+def test_get_all_with_condition(db_file, custom_class):
     objects = []
     for i in range(5):
         o = custom_class()
